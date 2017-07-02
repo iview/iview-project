cc8adbb182929c74c169e16b0ee686c170cf75f4
张林 <zhanglind@zbj.com>
2017-07-02 15:15:33 +0800

    增加多条消息依次展示的实现方法

--- src/views/index.vue
+++ src/views/index.vue
@@ -34,6 +34,11 @@
                 <h2>
                     <p>Welcome to your iView app!</p>
                     <Button type="ghost" @click="handleStart">Start iView</Button>
+                    <br>
+                    <br>
+                    <Button type="ghost" @click="handleRecursion">递归</Button>
+                    <Button type="ghost" @click="handlePromise">Promise</Button>
+                    <Button type="ghost" @click="handleAwait">Async/Await</Button>
                 </h2>
             </Col>
         </Row>
@@ -41,27 +46,76 @@
 </template>
 <script>
     export default {
+        data: function () {
+            return {
+                arr: [1, 2, 3, 4], //消息数组
+                time: 1.5 // 每条消息显示多少秒
+            }
+        },
         methods: {
             handleStart () {
-
-                // 多条消息依次显示
-                let arr = [1, 2, 3, 4];
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            },
+            handleRecursion () {
                 let vm = this;
                 (function next (len, curr) {
                     if (curr < len) {
                         vm.$Message.info({
-                            content: `第${arr[curr]}条信息`,
+                            content: `第${vm.arr[curr]}条消息`,
+                            duration: vm.time,
                             onClose: _ => {
                                 next(len, curr + 1);
                             }
                         });
                     }
-                })(arr.length, 0);
-                
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
-                });
+                })(vm.arr.length, 0);
+            },
+            handlePromise () {
+                let vm = this
+                vm.promiseForEach(vm.arr, (curr) => {
+                    return new Promise((resolve, reject) => {
+                        vm.$Message.info({
+                            content: `第${curr}条消息`,
+                            duration: vm.time
+                        });
+                        setTimeout(() => {
+                            return resolve();
+                        }, vm.time * 1000);
+                    })
+                })
+            },
+            async handleAwait () {
+                let vm = this;
+                for (let i = 0; i < vm.arr.length; i++) {
+                    let number = vm.arr[i];
+                    await new Promise((resolve, reject) => {
+                        vm.$Message.info({
+                            content: `第${number}条消息`,
+                            duration: vm.time
+                        });
+                        setTimeout(() => {
+                            return resolve();
+                        }, vm.time * 1000);
+                    })
+                }
+            },
+            promiseForEach (arr, cb) {
+                let realResult = []
+                let result = Promise.resolve()
+                arr.forEach(item => {
+                    result = result.then(() => {
+                        return cb(item).then((res) => {
+                            realResult.push(res)
+                        })
+                    })
+                })
+
+                return result.then(() => {
+                    return realResult
+                })
             }
         }
     }
