f3ccba79fb949095a77eae870f2423c459861c73
wuxingsheng <wuxingsheng>
2018-02-24 16:58:18 +0800

    解决build后打开页面空白问题

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ Vue.use(iView);
 
 // 路由配置
 const RouterConfig = {
-    mode: 'history',
+    mode: 'hash',
     routes: Routers
 };
 const router = new VueRouter(RouterConfig);
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -36,7 +36,7 @@ module.exports = merge(webpackBaseConfig, {
             }
         }),
         new HtmlWebpackPlugin({
-            filename: '../index_prod.html',
+            filename: './index_prod.html',
             template: './src/template/index.ejs',
             inject: false
         })
