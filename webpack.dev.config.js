f87dd2fc8d963a2f71edb29e5eba5617076fe7b6
王鹏 <peng.wang09@hand-china.com>
2019-08-01 10:56:02 +0800

    [fix]修复项目无法正常npm run init

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,7 +6,8 @@ const webpackBaseConfig = require('./webpack.base.config.js');
 const fs = require('fs');
 
 fs.open('./src/config/env.js', 'w', function (err, fd) {
-    const buf = 'export default "development";';
+    // const buf = 'export default "development";';
+    const buf = Buffer.from('export default "development";');
     fs.write(fd, buf, 0, buf.length, 0, function (err, written, buffer){});
 });
 
